eac065ddcb7e3aaa813e37b9443e5daf31731ff3
david072 <david.g.ganz@gmail.com>
2020-09-04 11:13:18 +0200

    Current code upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:stockwaage_app/screens/home/HomeDrawer.dart';
 import 'package:stockwaage_app/screens/login/login.dart';
